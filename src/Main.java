81b772483d0ac20f2d300c58dacf1c885594de27
AndreiPavlov1988 <ai_pavlov@mail.ru>
2025-12-07 19:54:45 +0300

    Сlass2

--- src/Main.java
+++ src/Main.java
@@ -33,4 +33,50 @@ class Product {
                 Objects.equals(category, other.category);
     }
 }
+// 2. Класс заказа
+class Order {
+    private String customer;
+    private Product[] basket;
+
+    public Order(String customer, Product[] basket) {
+        this.customer = customer;
+        this.basket = basket;
+    }
+
+    @Override
+    public String toString() {
+        return "Заказ[клиент=" + customer +
+                ", корзина=" + Arrays.toString(basket) + "]";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Order other = (Order) obj;
+
+        // Проверяем имя клиента
+        if (!Objects.equals(customer, other.customer)) {
+            return false;
+        }
+
+        // Проверяем корзину
+        if (basket == null && other.basket == null) return true;
+        if (basket == null || other.basket == null) return false;
+        if (basket.length != other.basket.length) return false;
+
+        // Сравниваем каждый товар
+        for (int i = 0; i < basket.length; i++) {
+            Product p1 = basket[i];
+            Product p2 = other.basket[i];
+
+            if (!Objects.equals(p1, p2)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
 
